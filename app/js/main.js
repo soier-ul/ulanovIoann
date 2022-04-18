55b9923457bf4f1a46987c82e3c9eb115e5c0fed
morj <68545141+soier-ul@users.noreply.github.com>
2022-04-18 17:31:41 +0300

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
 $(function () {
 
+  $('.header__dropdown-button').on('click', function () {
+    $('.header__dropdown-list').toggleClass('header__dropdown-list--active');
+
+    document.addEventListener('click', (e) => {
+      if (!e.target.closest('.header__dropdown-button, .header__dropdown-list')) {
+        $('.header__dropdown-list').removeClass('header__dropdown-list--active');
+      }
+    });
+  });
 
 
-  
 });

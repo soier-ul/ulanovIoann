518b06e734e29d9b4fd2a26081354029e7b36198
morj <68545141+soier-ul@users.noreply.github.com>
2022-04-18 19:06:52 +0300

    added address section

--- app/js/main.js
+++ app/js/main.js
@@ -11,3 +11,30 @@ $(function () {
   });
 
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelector('.addresses__tabs');
+  const tabsBtn = document.querySelectorAll('.addresses__btn');
+  const tabsContent = document.querySelectorAll('.addresses__content');
+
+  if (tabs) {
+    tabs.addEventListener('click', (e) => {
+      if (e.target.classList.contains('addresses__btn')) {
+        const tabsPath = e.target.dataset.tabsPath;
+        tabsHandler(tabsPath);
+      }
+    });
+  }
+
+  const tabsHandler = (path) => {
+    tabsBtn.forEach(el => {
+      el.classList.remove('addresses__btn--active')
+    });
+    document.querySelector(`[data-tabs-path="${path}"]`).classList.add('addresses__btn--active');
+
+    tabsContent.forEach(el => {
+      el.classList.remove('addresses__content--active')
+    });
+    document.querySelector(`[data-tabs-target="${path}"]`).classList.add('addresses__content--active');
+  };
+});

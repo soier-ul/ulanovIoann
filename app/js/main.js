0b980d613959143f76d96482e92e30645bb213ed
morj <68545141+soier-ul@users.noreply.github.com>
2022-04-19 13:30:54 +0300

    added faq and map desctions

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,41 @@ $(function () {
     });
   });
 
+  $('.accordion__btn').on('click', function () {
+    $(this).toggleClass('accordion__btn--active');
+    $('.accordion__content').toggleClass('accordion__content--active');
+    $(this).next('.accordion__content').slideToggle(350);
+  });
+
+  $('.map__current').on('click', function () {
+    $('.map__list').toggleClass('map__list--active');
+
+    document.addEventListener('click', (e) => {
+      if (!e.target.closest('.map__current, .map__list')) {
+        $('.map__list').removeClass('map__list--active');
+      }
+    });
+  });
+
 });
+
+function initMap() {
+  const pos = {
+    lat: 60.05096296918317,
+    lng: 30.382495863583244
+  };
+  const opt = {
+    center: pos,
+    zoom: 18
+  };
+
+  const map = new google.maps.Map(document.getElementById('map'), opt);
+
+  const marker = new google.mapsMarker({
+    position: pos,
+    map: map,
+  });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelector('.addresses__tabs');
